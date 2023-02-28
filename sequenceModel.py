009045f2cf482fb905ef41342d6015dfe192e8de
Mahdi <44380968+mbonyani@users.noreply.github.com>
2023-02-28 09:57:29 +0330

    Update sequenceModel.py

--- sequenceModel.py
+++ sequenceModel.py
@@ -150,29 +150,29 @@ class SequenceModel(Model):
         mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
         return mask
 
-    def compute_selfattention(self,transformer_encoder,x,mask,src_key_padding_mask,i_layer,d_model,num_heads):
+    def compute(self,transformer_encoder,x,mask,src_key_padding_mask,i_layer,d_model,num_heads):
         print("FFFFFFFF",x.shape)
         h = F.linear(x.float(), transformer_encoder.layers[i_layer].self_attn.in_proj_weight.float(), bias=transformer_encoder.layers[i_layer].self_attn.in_proj_bias.float())
         qkv = h.reshape(x.shape[0], x.shape[1], num_heads, 3 * d_model//num_heads)
-        qkv = qkv.permute(0, 2, 1, 3)  # [Batch, Head, SeqLen, Dims]
-        q, k, v = qkv.chunk(3, dim=-1) # [Batch, Head, SeqLen, d_head=d_model//num_heads]
-        attn_logits = torch.matmul(q, k.transpose(-2, -1)) # [Batch, Head, SeqLen, SeqLen]
+        qkv = qkv.permute(0, 2, 1, 3)  
+        q, k, v = qkv.chunk(3, dim=-1) 
+        attn_logits = torch.matmul(q, k.transpose(-2, -1)) 
         d_k = q.size()[-1]
         attn_probs = attn_logits / math.sqrt(d_k)
-        # combining src_mask e.g. upper triangular with src_key_padding_mask e.g. columns over each padding position
+        
         combined_mask = torch.zeros_like(attn_probs)
         if mask is not None:
-            combined_mask += mask.float() # assume mask of shape (seq_len,seq_len)
+            combined_mask += mask.float() 
         if src_key_padding_mask is not None:
             combined_mask += src_key_padding_mask.float().unsqueeze(1).unsqueeze(1).repeat(1,num_heads,x.shape[1],1)
-            # assume shape (batch_size,seq_len), repeating along head and line dimensions == "column" mask
+            
         combined_mask = torch.where(combined_mask>0,torch.zeros_like(combined_mask)-float("inf"),torch.zeros_like(combined_mask))
-        # setting masked logits to -inf before softmax
+        
         attn_probs += combined_mask
         attn_probs = F.softmax(attn_probs, dim=-1)
         return attn_logits,attn_probs
 
-    def extract_selfattention_maps(self,x,mask,src_key_padding_mask):
+    def extract_maps(self,x,mask,src_key_padding_mask):
         attn_logits_maps = []
         attn_probs_maps = []
         num_layers = self.transformer_encoder.num_layers
@@ -184,16 +184,15 @@ class SequenceModel(Model):
         x = self.pos_encoder(emb)
         with torch.no_grad():
             for i in range(num_layers):
-                # compute attention of layer i
+               
                 h = x.clone()
                 if norm_first:
                     h = self.transformer_encoder.layers[i].norm1(h)
-                # attn = transformer_encoder.layers[i].self_attn(h, h, h,attn_mask=mask,key_padding_mask=src_key_padding_mask,need_weights=True)[1]
-                # attention_maps.append(attn) # of shape [batch_size,seq_len,seq_len]
-                attn_logits,attn_probs = self.compute_selfattention(self.transformer_encoder,h,mask,src_key_padding_mask,i,d_model,num_heads)
-                attn_logits_maps.append(attn_logits) # of shape [batch_size,num_heads,seq_len,seq_len]
+
+                attn_logits,attn_probs = self.compute(self.transformer_encoder,h,mask,src_key_padding_mask,i,d_model,num_heads)
+                attn_logits_maps.append(attn_logits) 
                 attn_probs_maps.append(attn_probs)
-                # forward of layer i
+               
                 x = self.transformer_encoder.layers[i](x,src_mask=mask,src_key_padding_mask=src_key_padding_mask)
         return attn_logits_maps,attn_probs_maps
 
